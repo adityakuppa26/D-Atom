db3babe31add236f510ecfcaf88b0e5c705909cf
sushanth678 <sushanthsamala678@gmail.com>
2016-09-19 10:31:05 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -21,7 +21,12 @@ def compress(file_name,path='./'):
   else :
     print('This file format is not supported')
     return -1
-
+def decompress(file_name,path='./'):
+    if file_name.lower().endswith('.zlib')
+        pass
+    else :
+        print('This file format is not supported')
+        return -1
 o=input('Select the desired option \n 1.Compress a file. \n 2.Decompress a file. \n 3.Check if the file is already compressed. \n ')
 p=raw_input('Please give the file path \n')
 result=compress(filename_path(p),p)
